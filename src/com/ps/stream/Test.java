54fa97e26b019c305244928a1f5460e2a7495e52
Pushpendra Singh <pushpendrasingh@Pushpendras-Air.lan>
2025-03-16 01:11:40 -0500

    adding two more operations

--- src/com/ps/stream/Test.java
+++ src/com/ps/stream/Test.java
@@ -19,18 +19,36 @@ public class Test {
 		String[] ar = new String[] { "abc", "pqr", "xyz" };
 
 		convertArrayOfStringToUpperCase(ar);
+		
+		Integer[] nums= {1,2,3,4,5,6,7,8,9};
+		filterEvenNumber(nums);
+		
+		sumOfAllNumInList(nums);
 	}
 
-	// Convert List of Strings to Uppercase
+	//1.1 Convert List of Strings to Uppercase
 
 	private static void convertListOfStringToUpperCase(List<String> list) {
 		list.stream().map(s -> s.toUpperCase()).forEach(System.out::println);
 	}
 
-//	Convert Array of Strings to Uppercase
+   //1.2	Convert Array of Strings to Uppercase
 	private static void convertArrayOfStringToUpperCase(String[] ar) {
 		List<String> list = Arrays.asList(ar);
 		convertListOfStringToUpperCase(list);
 	}
+	
+	// 2-Find Even Numbers from a List
+	private static void filterEvenNumber(Integer[] ar) {
+		List<Integer> list = Arrays.asList(ar);
+		list.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+	}
 
+	// 3-Sum of All Elements in a List
+	private static void sumOfAllNumInList(Integer[] ar) {
+		List<Integer> list = Arrays.asList(ar);
+		Integer sum = list.stream().reduce(Integer::sum).get();
+		System.out.println(sum);
+	}
+	
 }
